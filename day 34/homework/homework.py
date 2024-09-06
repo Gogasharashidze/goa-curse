d0cb48c71579c21517f934ca0dfb5e385a8cb793
Gogasharashidze <gsharashidze352@gmail.com>
2024-09-06 13:53:01 +0400

    twenty-four commit

--- day 34/homework/homework.py
+++ day 34/homework/homework.py
@@ -6,17 +6,9 @@ def simple_calculator(num1, num2, operation):
     elif operation == 'multiplication':
         return num1 * num2
     elif operation == 'division':
-        if num2 == 0:
-            return "Error: Cannot divide by zero"
-        else:
+        if num2 != 0: 
             return num1 / num2
-    else:
-        return "Error: Invalid operation"
+ 
 
-
-print(simple_calculator(10, 5, 'addition'))      
-print(simple_calculator(10, 5, 'subtraction'))    
-print(simple_calculator(10, 5, 'multiplication')) 
-print(simple_calculator(10, 5, 'division'))     
-print(simple_calculator(10, 0, 'division'))       
-print(simple_calculator(10, 5, 'modulus')) 
+result = simple_calculator(10, 5, 'addition')
+print(result) 

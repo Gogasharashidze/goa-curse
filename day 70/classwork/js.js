741db799588b6bec7134c0a56cb006c4d0b56611
Gogasharashidze <gsharashidze352@gmail.com>
2025-01-10 20:27:24 +0400

    fourthy-two commit

---
+++ day 70/classwork/js.js
@@ -0,0 +1,20 @@
+let name = "my name is Goga"
+let surname = "my surname is Sharashidze"
+let age =12
+let car = " my fac car is lamborghini"
+console.log(name);
+alert(name);
+
+console.log(surname);
+alert(surname);
+
+console.log(age);
+alert(age);
+
+console.log(car);
+alert(car);
+
+
+console.log(name+surname+age+car)
+
+
---
+++ day 70/homework/brain.js
@@ -0,0 +1,55 @@
+// N1
+ 
+let num1 = parseFloat(prompt("Enter the first number:")); 
+let num2 = parseFloat(prompt("Enter the second number:")); 
+
+
+let ჯამი = num1 + num2;         
+let ნაკლებობა = num1 - num2;  
+let გამრავლება = num1 * num2;    
+let გაყოფა  = num1 / num2;    
+
+console.log("Sum: " + ჯამი);
+console.log("Difference: " + ნაკლებობა);
+console.log("Product: " + გამრავლება);
+console.log("Quotient: " + გაყოფა);
+
+
+
+
+
+
+// N2
+
+
+let name ="Goga"
+let surname = "Sharashidze"
+let age= 12
+
+
+
+// N3
+
+
+const arar ='sigma'
+const goa = "oriented"
+const dav= 'axali'
+
+
+
+// N4
+var goga ='sigmaa'
+var afa= "dmd"
+var dehd='gdde'
+
+// let ცვლადს განსაზღვრავს, რომლის მნიშვნელობის შეცვლა შესაძლებელია.
+
+
+
+// "const" ცვლადს განსაზღვრავს, რომლის მნიშვნელობის შეცვლა შეუძლებელია.
+
+
+
+// var კი შედარებით ძველია და უფრო დაუხვეწელი
+
+

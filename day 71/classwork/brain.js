97cca38ae51bfe3d96789284f5d257b287ca2f89
Gogasharashidze <gsharashidze352@gmail.com>
2025-01-12 18:37:36 +0400

    fourth-three commit

---
+++ day 71/classwork/brain.js
@@ -0,0 +1,23 @@
+console.log(30 * 24 * 60 * 60);
+let num1 = 30
+let num2 = 24
+let num3 = 60
+let num4 = 60
+
+console.log(num1 * num2 * num3 * num4)
+
+
+
+
+
+
+// N2 class
+
+
+let width = 8;
+let height = 14; 
+let area = width * height; 
+console.log(area); 
+
+
+
---
+++ day 71/homework/script.js
@@ -0,0 +1,19 @@
+function addToCart() {
+    alert('Added to Cart');
+  }
+  document.getElementById('myButton').addEventListener('click', function () {
+    console.log('Button is clicked!');
+    alert('Button is clicked!');
+  });
+  document.getElementById('myImage').addEventListener('click', function () {
+    console.log('Image is Clicked!');
+    alert('Image is Clicked!');
+  });
+  
+
+  
+  document.getElementById('myTable').addEventListener('click', function () {
+    console.log('Table is Clicked!');
+    alert('Table is Clicked!');
+  });
+  

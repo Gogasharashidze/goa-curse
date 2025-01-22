32f90a634701a5bfd594ebfb22bffb46a3d0f1fe
Gogasharashidze <gsharashidze352@gmail.com>
2025-01-22 21:05:26 +0400

    individual commit

---
+++ day 74/classwork/js.js
@@ -0,0 +1,14 @@
+let type = Number(prompt("emter type"))
+switch(type){
+    case 1:
+        console.log("Americano")
+        break
+    case 2:
+        console.log("Esspresso")
+        break
+        case 3:
+            console.log("Cappuccino")
+            default:
+               console.log("unknown")
+
+}

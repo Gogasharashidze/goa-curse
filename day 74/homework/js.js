12f52925fcee95ea3055974e0a961493ff777d77
Gogasharashidze <gsharashidze352@gmail.com>
2025-01-26 21:01:03 +0400

    fourtheenth--five commit

---
+++ day 74/homework/js.js
@@ -0,0 +1,52 @@
+
+let day = Number(prompt("emter num"))
+    switch (day) {
+        case 1:
+            console.log("monday")
+            break
+        case 2:
+            console.log("thursday")
+            break
+        case 3:
+           console.log("wednesday")
+           break
+        case 4:
+          console.log("thursday")
+          break
+        case 5:
+          console.log("firday")
+          break
+        case 6:
+           console.log("saturday")
+           break
+        case 7:
+          console.log("sunday")
+          break
+        default:
+            console.log("none")
+            break
+    }
+
+
+console.log(day)
+
+let weather =Number(prompt("emter type"))  
+    switch (weather) {
+        case 1:
+            console.log("sunny")
+            break
+        case 2:
+           console.log("rainy")
+           break
+        case 3:
+           console.log("ghrubliani")
+           break
+        case 4:
+           console.log("windy")
+           break
+        default:
+            console.log("none")
+            break
+    }
+
+console.log(weather)
